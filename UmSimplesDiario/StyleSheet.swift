24755d1557d859d95268b097145c2f1e01aff70c
viniciusmesquita <=>
2021-02-08 11:45:00 -0300

    Cria classe StyleSheet do aplicativo.

--- UmSimplesDiario/StyleSheet.swift
+++ UmSimplesDiario/StyleSheet.swift
@@ -5,4 +5,15 @@
 //  Created by Vinicius Mesquita on 08/02/21.
 //
 
-import Foundation
+import UIKit
+
+struct StyleSheet {
+    
+    enum Color {
+        static let primaryColor = UIColor.systemBlue
+    }
+    
+    enum Font {
+        static let primaryFont16 = UIFont.systemFont(ofSize: 16)
+    }
+}
