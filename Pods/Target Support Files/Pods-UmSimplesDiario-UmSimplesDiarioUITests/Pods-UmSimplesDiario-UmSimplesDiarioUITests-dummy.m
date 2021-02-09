2285d8853396ff65c02e6e2a4787043e7da41540
viniciusmesquita <=>
2021-02-09 13:28:16 -0300

    Remove todos os arquivos

--- Pods/Target Support Files/Pods-UmSimplesDiario-UmSimplesDiarioUITests/Pods-UmSimplesDiario-UmSimplesDiarioUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UmSimplesDiario_UmSimplesDiarioUITests : NSObject
-@end
-@implementation PodsDummy_Pods_UmSimplesDiario_UmSimplesDiarioUITests
-@end
--- Pods/Target Support Files/Pods-UmSimplesDiarioTests/Pods-UmSimplesDiarioTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UmSimplesDiarioTests : NSObject
-@end
-@implementation PodsDummy_Pods_UmSimplesDiarioTests
-@end
